6ec71853ad36b3a188078d9228537cdbf4e4ec38
SFKIT <sherjeel.khalid@gmail.com>
2019-01-28 23:44:44 +0000

    Commit after implementing first test case

--- Numbers/src/devops/numbers/NumberWords.java
+++ Numbers/src/devops/numbers/NumberWords.java
@@ -4,6 +4,11 @@ public class NumberWords {
 
 	public String toWords( int number ) {
 		String result = "" ;
+		
+		if ( number < 0 ) {
+			result = "Number out of range";
+		}
+		
 		return result.trim() ;
 	}
 
