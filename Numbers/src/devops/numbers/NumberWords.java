5046c0cdeaa162625735de1bdca69a889d3bcc02
SFKIT <sherjeel.khalid@gmail.com>
2019-01-29 00:19:27 +0000

    Check in after first set of unit tests for numbers less than 20

--- Numbers/src/devops/numbers/NumberWords.java
+++ Numbers/src/devops/numbers/NumberWords.java
@@ -1,12 +1,18 @@
 package devops.numbers;
 
 public class NumberWords {
+	
+	private final static String[] numbers = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", 
+			                                  "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 
 	public String toWords( int number ) {
 		String result = "" ;
 		
 		if ( number < 0 ) {
 			result = "Number out of range";
+		}
+		else {
+			result = numbers[number % 20];
 		}
 		
 		return result.trim() ;
--- Numbers/test/devops/numbers/NumberWordsTests.java
+++ Numbers/test/devops/numbers/NumberWordsTests.java
@@ -16,5 +16,27 @@ public class NumberWordsTests {
 	public void numberOutOfRangeReturnsError() {
 		Assert.assertEquals( "Error", "Number out of range", numberWords.toWords( -1 ) ) ;
 	}
+	
+	@Test
+	public void numberReturnsOne() {
+		Assert.assertEquals( "One", numberWords.toWords( 1 ) ) ;
+	}
+	
+	@Test
+	public void numberReturnsFive() {
+		Assert.assertEquals( "Five", numberWords.toWords( 5 ) ) ;
+	}
+
+	@Test
+	public void numberReturnsTen() {
+		Assert.assertEquals( "Ten", numberWords.toWords( 10 ) ) ;
+	}
+	
+	@Test
+	public void numberReturnsNineteen() {
+		Assert.assertEquals( "Nineteen", numberWords.toWords( 19 ) ) ;
+	}
+
+
 
 }
