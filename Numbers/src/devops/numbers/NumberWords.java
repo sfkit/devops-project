c1cb68319c7e830e7eb7ca91e8d9310174e917c5
SFKIT <sherjeel.khalid@gmail.com>
2019-01-29 13:21:39 +0000

    Commit to fix an issue with the output (missing space)

--- Numbers/src/devops/numbers/NumberWords.java
+++ Numbers/src/devops/numbers/NumberWords.java
@@ -28,9 +28,9 @@ public class NumberWords {
             	number /= 10;
             	
             	if (number == 0) return result.trim();
-            	if ( result.length() == 1 ) return ( numbers[number] + "Hundred" ).trim();
+            	if ( result.length() == 1 ) return ( numbers[number] + " Hundred" ).trim();
             	
-            	return ( numbers[number] + "Hundred and " + result ).trim();
+            	return ( numbers[number] + " Hundred and " + result ).trim();
             } 
 			
 		}
