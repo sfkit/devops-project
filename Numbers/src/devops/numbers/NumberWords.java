b47042450935cd92c4e6fa4e207d2d5f5f66ff5a
SFKIT <sherjeel.khalid@gmail.com>
2019-01-29 00:49:22 +0000

    Further code to add cases where numbers are between 20 and 99

--- Numbers/src/devops/numbers/NumberWords.java
+++ Numbers/src/devops/numbers/NumberWords.java
@@ -4,6 +4,7 @@ public class NumberWords {
 	
 	private final static String[] numbers = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", 
 			                                  "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+	private final static String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety");
 
 	public String toWords( int number ) {
 		String result = "" ;
@@ -12,7 +13,18 @@ public class NumberWords {
 			result = "Number out of range";
 		}
 		else {
-			result = numbers[number % 20];
+			
+            if (number % 100 < 20) {
+
+    			result = numbers[number % 100];
+    			number /= 100;
+            }
+            else {
+            	result = numbers[number % 10];
+            	result = tens[number % 10] + " " + result;
+            	number /= 10;
+            }
+			
 		}
 		
 		return result.trim() ;
